ffb9483b4e3496093498fb9d2e27e191edd3a0ed
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-06 06:38:14 +0300

    json file additions

--- src/components/BikeCollection.js
+++ src/components/BikeCollection.js
@@ -1,11 +1,15 @@
 import React from "react";
+import BikeCard from "./BikeCard";
 
-function BikeCollection() {
+function BikeCollection({bikes}) {
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {bikes.map((bike) =>(
+          <BikeCard key={bike.id} bike={bike}/>
+        ))}
         Collection of all bikes
       </div>
     </div>
--- src/components/BikePage.js
+++ src/components/BikePage.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import YourBikeCollection from "./YourBikeCollection";
 import BikeCollection from "./BikeCollection";
 
 function BikePage() {
   //start here with your code for step one
+  const[bikeData,setBikeData]=useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:8002/bots")
+    .then((res)=> res.json())
+    .then((data)=> setBikeData(data));
+  },[]);
 
   return (
     <div>
       <YourBikeCollection />
-      <BikeCollection />  
+      <BikeCollection bikes={bikeData}/>  
     </div>
   )
 }
