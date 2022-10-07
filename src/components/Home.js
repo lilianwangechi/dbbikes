0ca9caaae080ff200e03b39bd2fbcd4b88321516
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-07 15:50:06 +0300

    add final readme.md file

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,15 @@ import React from "react";
 function Home() {
   return (
     <>
+    <div id="home">
     <h1>Welcome To Dopaminaries</h1>
     <p>Stop dreaming start riding</p>
+    <main>
+		<h1 id="headline">A Cyclist Paradise.</h1>
+		<h2 id="about">We speak your language of getting everything cycling under one roof.</h2>
+		{/* <button class="purple-button">Contact Us</button> */}    
+	</main>
+  </div>
     </>
   );
 }
